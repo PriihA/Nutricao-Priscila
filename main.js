c8b37505fe7f7f4b679b6ed167fc4f5069905498
Priscila Aline Correa Batista <133381615+PriihA@users.noreply.github.com>
2024-09-20 12:12:02 -0300

    20.09

---
+++ main.js
@@ -0,0 +1,11 @@
+
+alert("Oi")
+console.log("Oiieeee");
+var titulo = document.querySelector("h1");
+titulo.textContent = "Priih Nutrição";
+
+var trPaulo = document.querySelector("#paulo");
+console.log(trPaulo);
+
+var.tdPeso = trpaulo.querySelector(".info-peso");
+tdPeso.textContent = "150"
